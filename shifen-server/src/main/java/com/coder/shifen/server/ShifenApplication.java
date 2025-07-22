7685277b64673ff5e3476f49b8de3c27c392e0fb
shifen11 <1586653872@qq.com>
2025-07-22 15:37:43 +0800

    init

---
+++ shifen-server/src/main/java/com/coder/shifen/server/ShifenApplication.java
@@ -0,0 +1,13 @@
+package com.coder.shifen.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShifenApplication.class, args);
+    }
+
+}
---
+++ shifen-server/src/test/java/com/coder/shifen/server/ShifenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coder.shifen.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShifenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
